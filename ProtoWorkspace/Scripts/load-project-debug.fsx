0201b8544bf5b97d613e4039d1b5b7037b71e08c
Jared Hester <jared.hester@gmail.com>
2016-11-01 23:43:07 -0400

    update load scripts

--- ProtoWorkspace/Scripts/load-project-debug.fsx
+++ ProtoWorkspace/Scripts/load-project-debug.fsx
@@ -1,6 +1,6 @@
 // Warning: generated file; your changes could be lost when a new file is generated.
 #I __SOURCE_DIRECTORY__
-#load "load-references-release.fsx"
+#load "load-references-debug.fsx"
 #load "../Prelude.fs"
       "../Extensions.fs"
       "../Environment.fs"
